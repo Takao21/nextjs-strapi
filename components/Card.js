e40b7830f3f72ec5b48b384a979f117e25676493
Takao21 <developer.aungkaungkhant@gmail.com>
2022-03-06 05:18:51 +0630

    api connected and styled cards

---
+++ components/Card.js
@@ -0,0 +1,50 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+const Card = ({ movie }) => {
+  const { API_URL } = process.env;
+  return (
+    <CardStyled>
+      <div className="poster">
+        <img
+          src={API_URL + movie.attributes.poster.data.attributes.url}
+          alt="movie poster"
+          layout="fill"
+        />
+      </div>
+      <div className="body">
+        <h3>{movie.attributes.title}</h3>
+        <p dangerouslySetInnerHTML={{ __html: movie.attributes.description }} />
+      </div>
+    </CardStyled>
+  );
+};
+
+const CardStyled = styled.div`
+  width: 400px;
+  border: 1px solid #cccccc;
+  margin-top: 50px;
+  border-radius: 20px;
+  overflow: hidden;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+
+  .poster {
+    img {
+      width: 100%;
+    }
+  }
+
+  .body {
+    padding: 20px;
+
+    h3 {
+      margin-bottom: 20px;
+    }
+    p {
+      color: #666666;
+      line-height: 1.5;
+    }
+  }
+`;
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import fetch from "isomorphic-unfetch";
+import Card from "../components/Card";
 
 const Home = ({ movies }) => {
-  console.log(movies);
+  console.log(movies.data);
   return (
-    <div className="Home">
-      <h1>I&apos;m HomePage.</h1>
+    <div className="container">
+      {movies.data.map((movie) => (
+        <Card key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 };
 
 export const getServerSideProps = async () => {
   const { API_URL } = process.env;
-  const res = await fetch(`${API_URL}/api/movies`);
+  console.log("api", API_URL);
+  const res = await fetch(`${API_URL}/api/movies?populate=*`);
   const data = await res.json();
   return {
     props: {
