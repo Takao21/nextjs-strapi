a0704830ffe79883f0622ecfa1c044bc3adfcb3e
Takao21 <developer.aungkaungkhant@gmail.com>
2022-03-06 04:05:29 +0630

    fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-export default function Home() {
+import fetch from "isomorphic-unfetch";
+
+const Home = ({ movies }) => {
+  console.log(movies);
   return (
     <div className="Home">
       <h1>I&apos;m HomePage.</h1>
     </div>
   );
-}
+};
+
+export const getServerSideProps = async () => {
+  const { API_URL } = process.env;
+  const res = await fetch(`${API_URL}/api/movies`);
+  const data = await res.json();
+  return {
+    props: {
+      movies: data,
+    },
+  };
+};
+
+export default Home;
