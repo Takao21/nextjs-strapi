435610f470561e42a4550d532bbf13fa0b0a2044
Takao21 <developer.aungkaungkhant@gmail.com>
2022-03-05 20:42:14 +0630

    removed indexjs boilerplate, added emotion/react

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Header from "components/Header";
 import "../styles/globals.css";
+import { ThemeProvider } from "emotion-theming";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Header />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
