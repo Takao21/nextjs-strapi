ab9443fd8a4886ebecec15d23521725f55c2233f
Takao21 <developer.aungkaungkhant@gmail.com>
2022-03-06 02:36:39 +0630

    logo, logo-text and style

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,33 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { rem } from "polished";
 
 const Header = () => {
-  return <HeaderStyled>Now I AM a real Header.</HeaderStyled>;
+  return (
+    <HeaderStyled>
+      <div className="container">
+        <div className="logo">
+          <img src="/images/logo.svg" alt="Sites logo" />
+          <span className="logo-text">Next Movies</span>
+        </div>
+      </div>
+    </HeaderStyled>
+  );
 };
 
 const HeaderStyled = styled.header`
-  background: ${({ theme }) => theme.colors.primary};
+  background: #efefef;
+  padding: 20px;
+  .logo {
+    display: flex;
+    align-items: center;
+    .logo-text {
+      color: #333333;
+      font-weight: bold;
+      font-size: ${rem(20)};
+      margin-left: 20px;
+    }
+  }
 `;
 
 export default Header;
